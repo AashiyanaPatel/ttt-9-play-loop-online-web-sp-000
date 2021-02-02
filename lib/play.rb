a0e204c13f4d846f7d4b02e84e13cfb6ac6fbe1f
Aashiyana Patel <aashiyanapatel@rocketmail.com>
2021-02-02 05:07:31 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -28,11 +28,9 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
-    move(board, index)
-    display_board(board)
+     move(board, index)
+     display_board(board)
   else
     turn(board)
   end
-end
 
-# Define your play method below
